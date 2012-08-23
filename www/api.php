a12d05fd08b6a0442a13ece16372d6a5caf4b385
F. Kooman <fkooman@tuxed.net>
2012-08-23 21:16:23 +0200

    update summerschool to use new REST stuff from HttpRequest for now

--- www/api.php
+++ www/api.php
@@ -33,48 +33,46 @@ try {
     }
     $rs->verifyAuthorizationHeader($authorizationHeader);
 
-    $rs->requireScope("grades");
-
     $response->setHeader("Content-Type", "application/json");
 
-    if($request->matchRest("GET", "grades", TRUE)) {
-        if($request->getResource() !== $rs->getResourceOwnerId() && "@me" !== $request->getResource()) {
+    $request->matchRest("GET", "/grades/:id", function($id) use ($rs, $response, $grades) {
+        $rs->requireScope("grades");
+        if($id !== $rs->getResourceOwnerId() && $id !== "@me") {
             throw new ApiException("forbidden", "resource does not belong to authenticated user");
         }
         if(!array_key_exists($rs->getResourceOwnerId(), $grades)) {
             throw new ApiException("not_found", "student does not have any grades");
         }
         $response->setContent(json_encode($grades[$rs->getResourceOwnerId()], JSON_FORCE_OBJECT));
-    } else {
-        throw new ApiException("invalid_request", "unsupported collection or resource request");
-    }
+    });
 
+    $request->matchRestDefault(function($methodMatch, $patternMatch) use ($request, $response) {
+        if(in_array($request->getRequestMethod(), $methodMatch)) {
+            if(!$patternMatch) {
+                throw new ApiException("not_found", "resource not found");
+            }
+        } else {
+            throw new ApiException("method_not_allowed", "request method not allowed");
+        }
+    });
+
+} catch (VerifyException $e) {
+    $response = new HttpResponse();
+    $response->setStatusCode($e->getResponseCode());
+    $response->setHeader("WWW-Authenticate", sprintf('Bearer realm="Resource Server",error="%s",error_description="%s"', $e->getMessage(), $e->getDescription()));
+    $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+    error_log($e->getLogMessage());
+} catch (ApiException $e) {
+    $response = new HttpResponse();
+    $response->setStatusCode($e->getResponseCode());
+    $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+    error_log($e->getLogMessage());
 } catch (Exception $e) {
-    switch(get_class($e)) {
-        case "Tuxed\\OAuth\\VerifyException":
-            $response = new HttpResponse();
-            $response->setStatusCode($e->getResponseCode());
-            $response->setHeader("WWW-Authenticate", sprintf('Bearer realm="Resource Server",error="%s",error_description="%s"', $e->getMessage(), $e->getDescription()));
-            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
-            error_log($e->getLogMessage());
-            break;
-
-        case "Tuxed\\OAuth\\ApiException":
-            $response = new HttpResponse();
-            $response->setStatusCode($e->getResponseCode());
-            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
-            error_log($e->getLogMessage());
-            break;
-
-        default:
-            // any other error thrown by any of the modules, assume internal server error
-            $response = new HttpResponse();
-            $response->setStatusCode(500);
-            $response->setContent(json_encode(array("error" => "internal_server_error", "error_description" => $e->getMessage())));
-            error_log($e->getMessage());
-            break;
-    }
-
+    // any other error thrown by any of the modules, assume internal server error
+    $response = new HttpResponse();
+    $response->setStatusCode(500);
+    $response->setContent(json_encode(array("error" => "internal_server_error", "error_description" => $e->getMessage())));
+    error_log($e->getMessage());
 }
 
 $response->sendResponse();
